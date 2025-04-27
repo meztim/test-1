9767c53e8c38b521f51a1e189e0621c4681b1eef
meztim <mezhonovtim@gmail.com>
2025-04-27 17:04:44 +0300

    Update main.py
    рандомаизер с помощью класса

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import random
 
-def wer(a, b):
-    c = random.randint(a,b)
-    return
+class Randomaizer:
+    def __init__(self,x, y):
+        self.border1 = x
+        self.border2 = y
+        self.value = self.__get_random_value()
 
-a = int(input('Введите число'))
-b = int(input('Введите число'))
-res = wer(a,b)
-print('Ваше число', res)
+    def __get_random_value(self):
+        return random.randint(self.border1, self.border2)
+
+    def get_value(self):
+        return self.value
+
+a = int(input("Введите число"))
+b = int(input("Введите число"))
+randomaizer = Randomaizer(a,b)
+result = randomaizer.get_value()
+print("Ваше число", result)
